9f915e7542770d5ca3c6a5f70aa281a200600c37
TheCedarPrince <jacobszelko@gmail.com>
2018-07-30 18:26:38 -0400

    Regex to shuffle a string to the end of a bigger string

--- _format_tools/regexer.py
+++ _format_tools/regexer.py
@@ -1,9 +1,21 @@
 import re
 
-with open("test.txt", "r") as f:
-    data = f.read().replace("\n", "")
+journals = open("publication_info/data/JOURNALS.txt")
 
-test = re.sub(r"(?i)(?=doi).*?(?=<)", "", data)
+journal_references = []
+for line in journals:
+    line = line.strip()
+    line = re.sub(r"\s+", " ", line)
+    if line[-1] == ".":
+        line = line[:-1]
+    url = re.search(r"(?i)(?=http).*?(?<=>)", line)
+    if url:
+        line = re.sub(r"(?i)(?=http).*?(?<=>)", "", line)
+        line = line + "|{}".format(url.group(0)[:-2])
+        journal_references.append(line)
+    else:
+        journal_references.append(line)
 
-results = open("results.txt", "w")
-results.write(test)
+with open("results.txt", "w") as results:
+    for i in range(0, len(journal_references)):
+        results.write(journal_references[i] + "\n")
