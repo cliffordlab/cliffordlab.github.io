293b26309afa7aad0e841cd8b255ae9635186126
TheCedarPrince <jacobszelko@gmail.com>
2018-08-03 13:52:07 -0400

    Parsing tool for mass editing of text storage

--- _format_tools/regexer.py
+++ _format_tools/regexer.py
@@ -1,21 +1,13 @@
-import re
+import regex as re
 
-journals = open("publication_info/data/JOURNALS.txt")
+journals = open("results.txt")
 
 journal_references = []
 for line in journals:
     line = line.strip()
-    line = re.sub(r"\s+", " ", line)
-    if line[-1] == ".":
-        line = line[:-1]
-    url = re.search(r"(?i)(?=http).*?(?<=>)", line)
-    if url:
-        line = re.sub(r"(?i)(?=http).*?(?<=>)", "", line)
-        line = line + "|{}".format(url.group(0)[:-2])
-        journal_references.append(line)
-    else:
-        journal_references.append(line)
+    line = re.sub(r"[a-z]\K\,(?!.*([a-z][\,]))", "|", line)
+    journal_references.append(line)
 
-with open("results.txt", "w") as results:
+with open("results_1.txt", "w") as results:
     for i in range(0, len(journal_references)):
         results.write(journal_references[i] + "\n")
