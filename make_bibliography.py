d60e3ea15191638e5067e9a56794786350da160a
James Weigle <jweigle@emory.edu>
2023-06-12 17:36:59 -0400

    Vastly improved publication generating, added most bibliography entries

--- make_bibliography.py
+++ make_bibliography.py
@@ -1,201 +1,303 @@
 #!/usr/bin/python3
 """
-Using the bibliographies under `bibliography/`, generate a Publications page
-for this website and save it to `publications.md`.
+Reading the bibliography file bibliography/gdclifford.bib,
+generate a Publications page for this website and save it to
+`publications.md`.
 
-The bibliographies are separated by category:
-    - bibliography/journals.bib (journals)
-    - bibliography/reviews_editorials.bib (reviews and editorials)
-    - bibliography/educational.bib (manuals, videos, computer programs, teaching aids)
-    - bibliography/conferences.bib (conference proceedings)
-    - bibliography/other.bib (miscellaneous)
-    - bibliography/theses.bib (theses)
+Print them in this order:
+    - article
+    - conference and inproceedings
+    - inbook
+    - book
+    - unpublished
+    - misc
+    - mastersthesis and phdthesis
 
 """
 import pybtex.database
-from inspect import getmembers
+import urllib.parse # for escaping special characters in a URL, so I can make
+                    # a download link...
+from datetime import datetime
+import codecs
+import latexcodec
 
-# categories of publication
-JOURNAL="Journal"
-REVIEW="Reviews and Editorials"
-EDUCATION="Manuals, Videos, Computer Programs, and Other Teaching Aids"
-CONFERENCE="Conferences"
-BOOK="Books and Chapters"
-OTHER="Other Materials"
-THESIS="Theses"
+bibliography="./bibliography/gdclifford.bib"
 
-bibliography = { JOURNAL: 
-                    "./bibliography/journals.bib",
-                 REVIEW:
-                    "./bibliography/reviews_editorials.bib",
-                 EDUCATION:
-                    "./bibliography/educational.bib",
-                 CONFERENCE:
-                    "./bibliography/conferences.bib",
-                 BOOK:
-                    "./bibliography/books.bib",
-                 OTHER:
-                    "./bibliography/other.bib",
-                 THESIS:
-                    "./bibliography/theses.bib" 
-                }
+def string2unicode(string):
+    """Convert latex special characters to unicode."""
+    return string.encode().decode('latex').replace('{','').replace('}','')
 
-def format_author(entry):
-    """Returns an author string in markdown format for a Vancouver-style citation."""
+def make_download_link(title, link_text, content):
+    """Return an HTML expression for a link saying `link_text` 
+    that downloads a text file with title `title` and content `content`.
+    """
+    return f'<a download="{title}" href="data:application/txt,{urllib.parse.quote(content)}">{link_text}</a>'
+
+def format_author_list(entry):
+    """Return an author list: Lastname FI, Lastname FI, Lastname FI."""
+
     def first_initials(author):
-        first_names = list(iter(author.first_names))
-        middle_names = list(iter(author.middle_names))
-        return "".join(n[0] for n in first_names + middle_names)
+        first_names=list(iter(author.rich_first_names))
+        middle_names=list(iter(author.rich_middle_names))
+        return "".join(n[0].render_as('markdown') for n in first_names + middle_names)
+
     def last_name(author):
-        return " ".join(author.last_names)
-    authors = list(iter(entry.persons['author']))
-#    if len(authors) >= 6:
-#        a = authors[0]
-#        author_string = f"{last_name(a)} {first_initials(a)} et al. "
-    if len(authors) == 1:
-        a = authors[0]
-        author_string = f"{last_name(a)} {first_initials(a)}. "
-    else:
-        first_authors=", ".join([f"{last_name(a)} {first_initials(a)}" for a in authors[:-1]])
-        last_author=f"{last_name(authors[-1])} {first_initials(authors[-1])}"
-        author_string = first_authors + ", and " + last_author + ". "
-    return author_string
+        return " ".join(n.render_as('markdown') for n in author.rich_last_names)
+        
+    authors=entry.persons['author']
+    return ", ".join(f"{last_name(a)} {first_initials(a)}" for a in authors)
 
 def format_title(entry):
-    """Return a string in markdown format
-    with the title, linking to the URL."""
-    title=entry.fields['title']
+    """Return a title that (if it has a link) links to the url."""
+    title=string2unicode(entry.fields['title'])
     if entry.type == 'inbook':
-        # then the title is a chapter title
-        title = "“" + title + "” in *" + entry.fields['booktitle'] + "*"
+        title = f"\"{title}\" in *{entry.fields['booktitle']}*"
     elif entry.type == 'book':
-        # the title is a book title
-        title = "*" + title + "*"
-    # otherwise just leave it alone
-
-    # make a url if we can
+        title = f"*{title}*"
+    # otherwise, just leave it alone 
+    
+    # make a URL if we can
     if 'url' in entry.fields:
         title = f"[{title}]({entry.fields['url']})"
-
+    
     return title
 
-def format_pages(pages):
-    """Add either p. or pp. to the page numbers."""
+def format_journal_name(entry):
+    """Format the name of the journal."""
+    return f"{string2unicode(entry.fields['journal'])}"
+
+def format_booktitle(entry):
+    """Format the name of the booktitle."""
+    return f"{string2unicode(entry.fields['booktitle'])}"
+
+def format_issue_number(entry):
+    """Format the issue number."""
+    reference = ""
+    if 'volume' in entry.fields:
+        reference=entry.fields['volume']    
+    if 'issue' in entry.fields:
+        iss = entry.fields['issue']
+        reference += f" ({iss})"
+    return reference
+
+def format_date(entry):
+    """Year, then possibly month, then possibly day."""
+    if 'date' in entry.fields:
+        date_obj = datetime.strptime(entry.fields['date'], "%Y-%m-%d")
+        return date_obj.strftime("%Y %B %d")
+    datestring=entry.fields['year']
+    
+    if 'month' in entry.fields:
+        datestring += f" {entry.fields['month']}"
+        if 'day' in entry.fields:
+            datestring += f" {entry.fields['day']}"
+    return datestring
+
+def format_doi_link(entry):
+    """Return the entry's DOI as a link."""
+    doi_number=entry.fields['doi']
+    link="https://doi.org/" + urllib.parse.quote(doi_number)
+    return f"[{doi_number}]({link})"
+
+def format_pages(entry):
+    """Format the page numbers."""
+    if 'pages' not in entry.fields:
+        return ""
+    pages=entry.fields['pages']
     if '-' in pages or ',' in pages:
         return 'pp. ' + pages
     else:
-        return 'p. ' + pages 
+        return 'p. ' + pages
 
-def format_journal_name(entry):
-    """Return a string in markdown format with
-    journal date;volume(issue):pages"""
-    journal = entry.fields['journal']
-    year = entry.fields['year']
-    if 'volume' in entry.fields:
-        vol = f"{entry.fields['volume']}"
-    else:
-        vol = ""
-    if 'issue' in entry.fields:
-        iss = f"({entry.fields['issue']})"
-    else:
-        iss = ""
-    voliss = "".join([vol,iss])
-    return " ".join([journal, year, voliss])
+def format_article(entry):
+    """Format the entry assuming it's an article."""
+    authors=format_author_list(entry)
+    title=format_title(entry)
+    journal=format_journal_name(entry)
+    date=format_date(entry)
+    numbers=format_issue_number(entry)
+    pages=format_pages(entry)
+    
+    reference=f"{authors}. {title}. {journal} {date}"
+    if numbers != "":
+        reference += f"; {numbers}"
+    if pages != "":
+        reference += f": {pages}"
+    reference += "."
 
-def make_download_link(title, content):
-    """Return an HTML expression for a link that downloads a text file
-    with title `title` and content `content`."""
-    return ('<a download="{{title}}" href="data:application/txt,{{content}}">BibTeX entry</a>'.replace("{{title}}", title)).replace("{{content}}", content)
-
-def format_bibtex_entry(index, entry):
-    """Return a formatted bibtex entry as a download link."""
-    string = entry.to_string('bibtex')
-    string = (string.replace('"', "'")).replace('\n', '')
-    filename = next(iter(entry.persons['author'])).last_names[0] + "_" + entry.fields['year'] + "_" + f"{index}" + ".bib"
-    return make_download_link(filename, string)
-
-def format_journal_article(entry):
-    """Returns a correctly cited journal article."""
-    refstring = "".join([format_author(entry),
-                         format_title(entry),
-                         ". ",
-                         format_journal_name(entry)])
-
-    if 'pages' in entry.fields:
-        refstring += ", "+format_pages(entry.fields['pages'])
-    refstring += "."
     if 'doi' in entry.fields:
-        refstring += " DOI: "+entry.fields['doi']
-    return refstring
+        reference += f" {format_doi_link(entry)}."
+    
+    return reference
 
-def format_publisher(entry):
-    if 'address' in entry.fields:
-        return entry.fields['address'] + "; " + entry.fields['publisher']
+def format_conference(entry):
+    """Format the entry assuming it's a Conference citation."""
+    authors=format_author_list(entry)
+    title=format_title(entry)
+    if 'booktitle' in entry.fields:
+        booktitle=format_booktitle(entry)
     else:
-        return entry.fields['publisher']
+        booktitle=format_journal_name(entry)
+    date=format_date(entry)
+    numbers=format_issue_number(entry)
+    pages=format_pages(entry)
+    
+    reference=f"{authors}. {title}. {booktitle} {date}"
+    if numbers != "":
+        reference += f"; {numbers}"
+    if pages != "":
+        reference += f": {pages}"
+    reference += "."
 
+    if 'doi' in entry.fields:
+        reference += f" {format_doi_link(entry)}."
+    
+    return reference
+
+# format for inbook
+
+def format_inbook(entry):
+    """Format the entry assuming it's a citation for a chapter in a book."""
+    authors=format_author_list(entry)
+    title=format_title(entry)
+    booktitle=format_booktitle(entry)
+    date=format_date(entry)
+    numbers=format_issue_number(entry)
+    pages=format_pages(entry)
+    
+    reference=f"{authors}. {title}. {booktitle} {date}"
+    if pages != "":
+        reference += f": {pages}"
+    reference += "."
+
+    if 'doi' in entry.fields:
+        reference += f" {format_doi_link(entry)}."
+    
+    return reference
+
+# format for book
 def format_book(entry):
-    """Format a book or a chapter in a book as a citation."""
-    refstring = "".join([format_author(entry),
-                         format_title(entry),
-                         ". ",
-                         format_publisher(entry),
-                         "; ",
-                         entry.fields['year']])
-    if entry.type == 'inbook':
-        refstring += ". " + format_pages(entry.fields['pages'])
-    refstring += "."
-    return refstring
+    """author. title. publisher: date."""
+    authors=format_author_list(entry)
+    title=format_title(entry)
+    publisher=entry.fields['publisher']
+    date=format_date(entry)
+    
+    reference=f"{authors}. {title}. {date} {publisher}."
 
-def format_education(entry):
-    refstring = "".join([format_author(entry),
-                         format_title(entry),
-                        ". "])
-    if 'institution' in entry.fields:
-        refstring += entry.fields['institution'] + " "
-    if 'howpublished' in entry.fields:
-        refstring += entry.fields['howpublished'] + " "
-    refstring += entry.fields['year'] + '.'
-    return refstring
+    if 'doi' in entry.fields:
+        reference += f" {format_doi_link(entry)}."
+    
+    return reference
 
-def format_other(entry):
-    refstring="".join([format_author(entry),
-                      format_title(entry),
-                      ". ",
-                      entry.fields['howpublished'],
-                      ", ",
-                      entry.fields['year'],
-                      "."])
-    return refstring
+def format_incollection(entry):
+    return format_inbook(entry)
+
+def format_misc(entry):
+    """author. title. howpublished. date."""
+    author=format_author_list(entry)
+    title=format_title(entry)
+    howpublished=entry.fields['howpublished']
+    date=format_date(entry)
+    return f"{author}. {title}. {howpublished}. {date}."
 
 def format_thesis(entry):
-    refstring = "".join([format_author(entry),
-                         format_title(entry),
-                         ". ",
-                         entry.fields['type'],
-                         ": ",
-                         entry.fields['school'],
-                         ", ",
-                         entry.fields['year'],
-                         "."])
-    return refstring
+    """Author. Title. Type, year, school."""
+    author=format_author_list(entry)
+    title=format_title(entry)
+    school=entry.fields['school']
+    thesis_type=entry.fields['type']
+    year=entry.fields['year']
+    return f"{author}. {title}. {thesis_type}, {year}, {school}."
 
-def format_citation(publication_type, entry):
-    if publication_type == JOURNAL:
-        return format_journal_article(entry)
-    elif publication_type == REVIEW:
-        return format_journal_article(entry) # change this if needed
-    elif publication_type == BOOK:
-        return format_book(entry)
-    elif publication_type == EDUCATION:
-        return format_education(entry)
-    elif publication_type == CONFERENCE:
-        return format_journal_article(entry) # change this if needed
-    elif publication_type == OTHER:
-        return format_other(entry) 
-    elif publication_type == THESIS:
+def format_entry(entry):
+    """Format the given entry as a citation, depending on its type."""
+    if entry.type == 'article':
+        return format_article(entry)
+    if entry.type == 'thesis':
         return format_thesis(entry)
-    return "Not sure how to cite this yet."
+    if entry.type == 'book':
+        return format_book(entry)
+    if entry.type == 'inbook':
+        return format_inbook(entry)
+    if entry.type == 'incollection':
+        return format_incollection(entry)
+    if entry.type == 'conference' or entry.type == 'inproceedings':
+        return format_conference(entry)
+    return format_misc(entry)
+
+def add_section(file, title,publication_list):
+    """Add the section to publications.md"""
+    file.write(f"\n## {title}\n")
+    file.write("\n|--|--|--|\n")
+    for i, (key, entry) in publication_list:
+        citation=format_entry(entry)
+        link = make_download_link(f"{key}.bib",
+                                  "BibTeX entry",
+                                  entry.to_string('bibtex'))
+        file.write(f"| {i} | {citation} | {link} |\n")
+
+### ---------------------------
+### MAIN BEHAVIOR OF THE SCRIPT
+### ---------------------------
+
+bib_data = pybtex.database.parse_file(bibliography)
+
+publication_types={}
+
+# separate the bibliography by categories
+for (k,e) in bib_data.entries.items():
+    if e.type not in publication_types:
+        publication_types[e.type] = []
+    publication_types[e.type].append((k,e))
+
+# inproceedings same as conference; combine into conference
+if 'conference' not in publication_types:
+    publication_types['conference'] = []
+if 'inproceedings' in publication_types:
+    publication_types['conference'] += publication_types['inproceedings']
+    del publication_types['inproceedings'] 
+
+# combine books, chapters, incollection
+if 'book' not in publication_types:
+    publication_types['book'] = []
+if 'inbook' in publication_types:
+    publication_types['book'] += publication_types['inbook']
+    del publication_types['inbook']
+if 'incollection' in publication_types:
+    publication_types['book'] += publication_types['incollection']
+    del publication_types['incollection']
+
+# finally, number each category in reverse chronological order
+# (highest numbers first, lowest is 1)
+
+def get_date(entry):
+    if 'date' in entry.fields:
+        # try parsing as yyyy-mm-dd format
+        date = datetime.strptime(entry.fields['date'], '%Y-%m-%d')
+        return date.year, date.month, date.day
+    
+    # parse year, month, and day individually
+    year = int(entry.fields['year'])
+    month = 1
+    day = 1
+    
+    if 'month' in entry.fields:
+        try:
+            month = datetime.strptime(entry.fields['month'], '%b').month
+        except ValueError:
+            month = datetime.strptime(entry.fields['month'], '%B').month
+    
+    if 'day' in entry.fields:
+        day = int(entry.fields['day'])
+    
+    return year, month, day
+
+for t in publication_types:
+    # sort the entries by date
+    sorted_entries=sorted(list(publication_types[t]), key=lambda tup: get_date(tup[1]))
+    # print them most recent first, numbered in descending order
+    publication_types[t] = reversed(list(enumerate(sorted_entries, 1)))
 
 with open("./publications.md", "w") as file:
     file.write("""---
@@ -204,17 +306,9 @@ title: Publications
 permalink: /publications
 ---
 """)
-
-    for publication_type, filepath in bibliography.items():
-        print(f"Type: {publication_type}. Filepath: {filepath}")
-        file.write(f"\n## {publication_type}\n")
-        file.write("\n|--|--|--|\n")
-        bib_data = pybtex.database.parse_file(filepath)
-        # Number them in reverse chronological order by category
-        # (highest numbers first, lowest is 1)
-        entries=reversed(list((enumerate(sorted(list(bib_data.entries.items()), key=lambda tup: (tup[1].fields['year'])), 1))))
-#        entries=list(bib_data.entries.items()).sort(key=lambda tup: (tup[1]).fields['year'])
-        for i, (key, entry) in entries:
-            citation = format_citation(publication_type, entry)
-            link = format_bibtex_entry(i, entry)
-            file.write(f"| {i} | {citation} | {link} |\n")
+    
+    add_section(file,"Journal articles", publication_types['article'])
+    add_section(file,"Conferences", publication_types['conference'])
+    add_section(file,"Books and Chapters", publication_types['book'])
+    add_section(file,"Other Material", publication_types['misc'])
+    add_section(file,"Theses", publication_types['thesis'])
